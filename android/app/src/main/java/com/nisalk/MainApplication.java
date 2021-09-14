1adaecae38b6311a6cb7a4c4fe06ccff1a1d1cfe
andrewklab <karganov.k@gmail.com>
2021-09-14 07:06:18 +0300

    home

--- android/app/src/main/java/com/nisalk/MainApplication.java
+++ android/app/src/main/java/com/nisalk/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.reactlibrarynotificationsounds.NotificationSoundsPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.messaging.ReactNativeFirebaseMessagingPackage;
--- index.js
+++ index.js
@@ -2,20 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import notifee, { AndroidImportance, AndroidVisibility } from '@notifee/react-native';
-
-notifee.createChannel({
-  id: 'foreground-notifications',
-  name: 'Уведомления при открытом приложении',
-  badge: true,
-  bypassDnd: false,
-  description: 'Данный канал предназначен для уведомлений которые приходят когда это приложение открыто и оно не находится в свернутом соостоянии.',
-  sound: 'default',
-  lights: true,
-  vibration: true,
-  importance: AndroidImportance.HIGH,
-  visibility: AndroidVisibility.PUBLIC
-});
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('Message handled in the background!', remoteMessage);
